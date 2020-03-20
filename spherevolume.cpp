4323efc0221ece951eae9a169bc085f8bc8fb55e
camillezajac <camillezajac@gmail.com>
2020-03-20 02:46:22 -0700

    Update spherevolume.cpp

--- spherevolume.cpp
+++ spherevolume.cpp
@@ -9,18 +9,14 @@ double sphereVolume(double, const double, double);
 const double PI = 3.14; // global constant
 
 int main(){
-	// variable declaration
 	double radius, volume;
 	
-
-	// prompt for input
 	cout << "radius: " << endl;
 	cin >> radius;
 
 	// function call
 	volume = sphereVolume(radius, PI, volume);
 
-	// print result
 	cout << "Volume: " << volume << endl;
 
 	return 0;
